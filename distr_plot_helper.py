3b65e9f8052cd21183764acca121c518c3c6cd69
Becker Awqatty <bawqatty@mide.com>
2020-01-23 12:24:34 -0500

    added new distribution plot utility function, `index_jagged`
    - refactored `resize2D_to_rectangular` to use internally

--- distr_plot_helper.py
+++ distr_plot_helper.py
@@ -1,22 +1,35 @@
 import numpy as np
 
 
+def index_jagged(jagged, percentile_indices):
+    """
+    Takes a rectangular slice of a "jagged array". The percentiles dictate the
+    element positions in each jagged subarray, as a percentage of the full
+    subarray length. For each percentile and jagged subarray, the nearest
+    element in the subarray to the given percentile is used.
+    """
+    result = np.zeros((len(jagged), len(percentile_indices)))
+
+    for i, a in enumerate(jagged):
+        result[i] = np.asanyarray(a)[np.digitize(
+            percentile_indices,
+            np.linspace(0, 1, num=(len(a)+1))[1:-1],
+        )]
+
+    return result
+
+
 def resize2D_to_rectangular(jagged):
     """
     Takes a 2D "jagged array" (i.e., an iterable of 1D arrays) and creates a
     rectangular 2D numpy array by resampling each 1D array up to the same
     length.
     """
-    result = np.zeros((len(jagged), max(len(a) for a in jagged)))
-    new_item_positions_on_axis = np.linspace(0, 1, num=(2*result.shape[1]+1))[1:-1:2]
-
-    for i, a in enumerate(jagged):
-        result[i] = np.asarray(a)[np.digitize(
-            new_item_positions_on_axis,
-            np.linspace(0, 1, num=(len(a)+1))[1:-1],
-        )]
-
-    return result
+    jagged_len = max(len(a) for a in jagged)
+    return index_jagged(
+        jagged,
+        np.linspace(0, 1, num=(2*jagged_len+1))[1:-1:2]
+    )
 
 
 def make_stack_plot_array(distr, axis=0, max_percentiles=np.inf):
