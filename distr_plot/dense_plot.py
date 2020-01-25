413a0c130f7ceff01d64cb5023535bc659803b1f
CrepeGoat <awqatty.b@gmail.com>
2020-01-24 19:09:01 -0500

    switched order of return values from `bins`
    - matches `scipy.signals.spectrogram`

--- distr_plot/dense_plot.py
+++ distr_plot/dense_plot.py
@@ -6,6 +6,6 @@ import matplotlib.pyplot as plt
 
 def bins(array, segment_count):
     return (
+        np.linspace(0, 1, 2*segment_count+1, endpoint=True)[1:-1:2],
         np.split(array, np.linspace(0, 1, segment_count+1, endpoint=True)[1:-1]),
-        np.linspace(0, 1, 2*segment_count+1, endpoint=True)[1:-1:2]
     )
