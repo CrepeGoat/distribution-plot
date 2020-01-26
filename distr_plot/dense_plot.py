f66de395fd067504c07d57a803f646a44623081a
CrepeGoat <awqatty.b@gmail.com>
2020-01-26 15:20:46 -0500

    added docstring to `bins`

--- distr_plot/dense_plot.py
+++ distr_plot/dense_plot.py
@@ -5,6 +5,11 @@ import matplotlib.pyplot as plt
 
 
 def bins(array, segment_count):
+    """
+    Partitions an array into equal-ish-width bins. Returns
+    - the center quantile corresponding to each bin, and
+    - the samples in each bin
+    """
     return (
         np.linspace(0, 1, 2*segment_count+1, endpoint=True)[1:-1:2],
         np.split(array, np.linspace(0, 1, segment_count+1, endpoint=True)[1:-1]),
