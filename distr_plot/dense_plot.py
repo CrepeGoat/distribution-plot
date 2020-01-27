ae39e427c98572c65df12584005b9654517c9d80
CrepeGoat <awqatty.b@gmail.com>
2020-01-26 21:51:29 -0500

    copy/pasted code from "percentile plot" notebook

---
+++ distr_plot/dense_plot.py
@@ -0,0 +1,11 @@
+import distr_plot.plotter
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def bins(array, segment_count):
+    return (
+        np.split(array, np.linspace(0, 1, segment_count+1, endpoint=True)[1:-1]),
+        np.linspace(0, 1, 2*segment_count+1, endpoint=True)[1:-1:2]
+    )
