beb600ec32f4491a28297a7ff580dee9cea6bcef
CrepeGoat <awqatty.b@gmail.com>
2020-01-26 21:57:47 -0500

    refactored `segment_count` -> `bin_count`

--- distr_plot/dense_plot.py
+++ distr_plot/dense_plot.py
@@ -4,22 +4,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def bins(array, segment_count):
+def bins(array, bin_count):
     """
     Partitions an array into equal-ish-width bins. Returns
     - the center quantile corresponding to each bin, and
     - the samples in each bin
     """
     return (
-        np.linspace(0, 1, 2*segment_count+1, endpoint=True)[1:-1:2],
+        np.linspace(0, 1, 2*bin_count+1, endpoint=True)[1:-1:2],
         np.split(array, np.linspace(
-            0, len(array), segment_count+1, endpoint=True
+            0, len(array), bin_count+1, endpoint=True
         )[1:-1].astype(np.int64)),
     )
 
 
-def dense_plot(x, y, segment_count, max_quantiles=np.inf, **kwargs):
-    avg_indices, bin_arrays = bins(y, segment_count)
+def dense_plot(x, y, bin_count, max_quantiles=np.inf, **kwargs):
+    avg_indices, bin_arrays = bins(y, bin_count)
     distr_plot.plotter.distr(
         np.quantile(x, avg_indices),
         bin_arrays,
